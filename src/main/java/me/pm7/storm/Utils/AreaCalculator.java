e3fb9748840748fecb3b6de2a11ddb3eb25bddcd
_Pm7 <pm7wasnottaken@gmail.com>
2025-07-03 14:05:35 -0500

    fix stupid chunk unloader!!!

--- src/main/java/me/pm7/storm/Utils/AreaCalculator.java
+++ src/main/java/me/pm7/storm/Utils/AreaCalculator.java
@@ -10,7 +10,7 @@ import java.awt.Point;
 
 public class AreaCalculator {
     private static final int INACCURACY = 1; // improves performance at the cost of area accuracy (might be broken, works at 1)
-    public static final int BOUNDING_BOX_SIZE = 160;
+    public static final int BOUNDING_BOX_SIZE = 200;
     private static final int HALF_SIZE = BOUNDING_BOX_SIZE/INACCURACY/2;
 
     public static int getTotalArea(List<Location> centers) {
