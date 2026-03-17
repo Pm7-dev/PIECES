dca8c1536a54c699318a128a5f6a6a61c5d6b68e
_Pm7 <pm7wasnottaken@gmail.com>
2026-03-17 14:20:13 -0500

    add support for 1.21.9

--- src/main/java/me/pm7/blockstorm/Listener/ConnectionListener.java
+++ src/main/java/me/pm7/blockstorm/Listener/ConnectionListener.java
@@ -11,25 +11,13 @@ import java.util.UUID;
 public class ConnectionListener implements Listener {
     private static final BlockStorm plugin = BlockStorm.getPlugin();
 
-    private static final String sha1string = "3eb70f8020d0c11d653708fe6456e09ae23a628a";
-    private static final UUID uuid = UUID.fromString("0facade0-feed-cafe-fade-0decaf0beef0");
-    byte[] sha1 = null;
+    private static final UUID uuid = UUID.fromString("0facade1-feed-cafe-fade-0decaf0beef0");
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
 
-        if(sha1 == null) {
-            int len = sha1string.length();
-            byte[] data = new byte[len / 2];
-            for (int i = 0; i < len; i += 2) {
-                data[i / 2] = (byte) ((Character.digit(sha1string.charAt(i), 16) << 4)
-                        + Character.digit(sha1string.charAt(i+1), 16));
-            }
-            sha1 = data;
-        }
-
         Player p = e.getPlayer();
-        p.addResourcePack(uuid, "https://raw.githubusercontent.com/Pm7-dev/PIECES/refs/heads/main/storm_pack_1.21.6.zip", sha1, "hi there u need this :3", true);
+        p.addResourcePack(uuid, "https://github.com/Pm7-dev/PIECES/raw/refs/heads/main/storm_pack_1.21.9.zip", null, "hi there u need this :3", true);
 
         if(plugin.isStarted()) {
             plugin.getPieceKeeper().setImmunity(p, 240);
